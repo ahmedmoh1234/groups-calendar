a4d2dce53ab9a50727e85ec8ff42813dfffbb5d7
Ahmed <ahmedmoh123@hotmail.com>
2025-12-28 00:44:48 +0200

    Added VITE_BASE_PATH

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { mochaPlugins } from "@getmocha/vite-plugins";
 export default defineConfig({
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   plugins: [...mochaPlugins(process.env as any), react(), cloudflare()],
+  base : process.env.VITE_BASE_PATH || "/groups-calendar",
   server: {
     allowedHosts: true,
   },
